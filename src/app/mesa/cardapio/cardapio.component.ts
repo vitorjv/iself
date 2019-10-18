079386ac5e74cff02d79e903fb7b83535627d63c
Davi Sampaio <sampaiodev23@gmail.com>
2019-10-18 10:05:43 -0300

    consultas no firebase por tipo de item

--- src/app/mesa/cardapio/cardapio.component.ts
+++ src/app/mesa/cardapio/cardapio.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, IonList } from '@ionic/angular';
 import { ContaComponent } from '../conta/conta.component';
-
+import {firebaseConfig} from '../../../environments/environment';
 @Component({
   selector: 'app-cardapio',
   templateUrl: './cardapio.component.html',
@@ -10,16 +10,14 @@ import { ContaComponent } from '../conta/conta.component';
 export class CardapioComponent implements OnInit {
 
   activedView = 'entrada';
-
   constructor(public modalController: ModalController) {
-
   }
-
+  
   async presentModal() {
     const modal = await this.modalController.create({
       component: ItemDetail,
       componentProps: {
-        'firstName': 'Douglas',
+        'firstName': 'Davi',
         'lastName': 'Adams',
         'middleInitial': 'N'
       }
@@ -27,17 +25,44 @@ export class CardapioComponent implements OnInit {
     return await modal.present();
   }
 
+  async returnDishes(tipo) {
+    const firebase = require("firebase");
+    require("firebase/firestore");
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyB3nffUHpv_4_7ged3sWNZsZQf-81owOjw",
+        authDomain: "iself2.firebaseapp.com",
+        databaseURL: "https://iself2.firebaseio.com",
+        projectId: "iself2",
+        storageBucket: "",
+        messagingSenderId: "578892561584",
+        appId: "1:578892561584:web:aa1e1d30596a36a23c4f75"
+      });
+    }
+    var list = new Array;
+    var db = firebase.firestore();
+    db.collection("itens").where("tipo", "==", tipo)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+  }
+
   ngOnInit() {}
 
   toggleView(event) {
     this.activedView = event.detail.value;
   }
-
 }
 
 @Component({
   selector: 'item-detail',
-  template: '<h1>{{ firstName }}</h1>',
+  template: '<h1>{{ firstName }} {{ lastName }}</h1>',
   styles: ['./cardapio.component.scss'],
 })
 export class ItemDetail implements OnInit {
